b1634df49a17212269ef9088eea079753d381397
Minh Nguyen <minhco12@gmail.com>
2024-04-19 18:34:18 +0300

    update portfolio website

---
+++ .next/static/chunks/app_layout_tsx_85066a._.js
@@ -0,0 +1,19 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
+    "static/chunks/app_layout_tsx_85066a._.js",
+    {},
+]);
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+  "path": "static/chunks/app_layout_tsx_85066a._.js",
+  "chunks": [
+    "static/chunks/_3d8305._.css",
+    "static/chunks/node_modules_next_4e2aef._.js",
+    "static/chunks/node_modules_@codemirror_state_dist_index_9b015e.js",
+    "static/chunks/node_modules_@codemirror_view_dist_index_201017.js",
+    "static/chunks/node_modules_@codemirror_language_dist_index_cccc62.js",
+    "static/chunks/node_modules_@codesandbox_sandpack-react_dist_index_mjs_01064b._.js",
+    "static/chunks/node_modules_09d696._.js",
+    "static/chunks/app_59c0be._.js",
+    "static/chunks/node_modules_@codesandbox_sandpack-client_dist_1a3ef2._.js"
+  ],
+  "source": "dynamic"
+});
---
+++ .next/static/chunks/app_page_tsx_4d7366._.js
@@ -0,0 +1,15 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
+    "static/chunks/app_page_tsx_4d7366._.js",
+    {},
+]);
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+  "path": "static/chunks/app_page_tsx_4d7366._.js",
+  "chunks": [
+    "static/chunks/node_modules_next_b858bd._.js",
+    "static/chunks/node_modules_@fortawesome_fontawesome-svg-core_index_mjs_49f25b._.js",
+    "static/chunks/node_modules_@fortawesome_free-brands-svg-icons_index_mjs_660f2e._.js",
+    "static/chunks/node_modules_5e8290._.js",
+    "static/chunks/_b80a5b._.js"
+  ],
+  "source": "dynamic"
+});
